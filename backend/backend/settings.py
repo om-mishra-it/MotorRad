716e13c5edf1033eedb4463f3dd62a8e79c992a4
Om Mishra <om.mishra@bacancy.com>
2024-09-19 12:05:51 +0530

    Configured local postgres database and updated INSTALLED_APPS

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -37,6 +37,14 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "rest_framework",
+    "activity_logs",
+    "events",
+    "groups",
+    "notifications",
+    "social",
+    "users",
+    "vehicles"
 ]
 
 MIDDLEWARE = [
@@ -74,11 +82,16 @@ WSGI_APPLICATION = "backend.wsgi.application"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'motorrad',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
+
 
 
 # Password validation
@@ -115,3 +128,6 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+# Defining custom User model
+AUTH_USER_MODEL = 'users.User'
